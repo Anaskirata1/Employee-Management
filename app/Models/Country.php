645b9578b61ebf1c1a74b18216d2045f8661668e
Anas Kirata <anaskirata2@gmail.com>
2026-01-05 20:40:45 +0300

    Create Models

---
+++ app/Models/Country.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+    protected $fillable = ['country_id' , 'name'];
+
+    public function employees()
+     {
+        return $this->hasMany(Employee::class);
+     }
+
+     public function state()
+     {
+        return $this->hasMany(State::class);
+     }
+}
